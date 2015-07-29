119e8e49edef16547965a7a6c95af67bb815c082
TheoLeanse <theoleanse@gmail.com>
2015-07-29 13:16:57 +0100

    updates controller

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,3 +1,12 @@
-githubUserSearch.controller('GitUserSearchController', [function() {
-
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+  var vm = this;
+  var searchResource = $resource('https://api.github.com/search/users');
+  vm.doSearch = function() {
+    vm.searchResult = searchResource.get(
+      { q: vm.searchTerm,
+        access_token: githubSecret }
+    );
+  };
 }]);
+
+// '264601a743391608b4223c49a37cfd1b93dff5c5'
