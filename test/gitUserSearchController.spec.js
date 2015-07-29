7e56c0567351fbde1de8e165be5a8dbba63927ba
TheoLeanse <theoleanse@gmail.com>
2015-07-29 13:17:15 +0100

    updates unit test for controller

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -11,4 +11,34 @@ describe('GitUserSearchController', function () {
     expect(ctrl.searchResult).toBeUndefined();
     expect(ctrl.searchTerm).toBeUndefined();
   });
+
+  describe('when searching for a user', function() {
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?access_token=" + githubSecret + "&q=Hello")
+        .respond(
+          { items: items }
+        );
+    }));
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+    it('displays search results', function() {
+      ctrl.searchTerm = 'Hello';
+      ctrl.doSearch();
+      httpBackend.flush();
+      expect(ctrl.searchResult.items).toEqual(items);
+    });
+  });
 });
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -8,7 +8,8 @@ module.exports = function(config) {
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'js/**/*.js',
-      'test/**/*.spec.js'
+      'test/**/*.spec.js',
+      'secrets/**/*.js'
     ],
     exclude: [],
     preprocessors: {},
